72147d1e651f376b2edd3fc75c4d3517f17a2594
Robert Ladstaetter <rladstaetter@gmail.com>
2015-11-26 21:12:39 +0100

    tic tac toe, initial commit

---
+++ src/main/scala/fhj/swengb/assignments/ttt/rladstaetter/TicTacToe.scala
@@ -0,0 +1,148 @@
+package fhj.swengb.assignments.ttt.rladstaetter
+
+import scala.collection.Set
+
+/**
+  * models the different moves the game allows
+  *
+  * each move is made by either player a or player b.
+  */
+sealed trait TMove {
+  def idx: Int
+}
+
+case object TopLeft extends TMove {
+  override def idx: Int = 0
+}
+
+case object TopCenter extends TMove {
+  override def idx: Int = 1
+}
+
+case object TopRight extends TMove {
+  override def idx: Int = 2
+}
+
+case object MiddleLeft extends TMove {
+  override def idx: Int = 3
+}
+
+case object MiddleCenter extends TMove {
+  override def idx: Int = 4
+}
+
+case object MiddleRight extends TMove {
+  override def idx: Int = 5
+}
+
+case object BottomLeft extends TMove {
+  override def idx: Int = 6
+}
+
+case object BottomCenter extends TMove {
+  override def idx: Int = 7
+}
+
+case object BottomRight extends TMove {
+  override def idx: Int = 8
+}
+
+
+/**
+  * for a tic tac toe game, there are two players, player A and player B
+  */
+sealed trait Player
+
+case object PlayerA extends Player
+
+case object PlayerB extends Player
+
+object TicTacToe {
+
+  /**
+    * creates an empty tic tac toe game
+    * @return
+    */
+  def apply(): TicTacToe = ???
+
+  /**
+    * For a given tic tac toe game, this function applies all moves to the game.
+    *
+    * @param t
+    * @param moves
+    * @return
+    */
+  def play(t: TicTacToe, moves: Seq[TMove]): TicTacToe = ???
+
+  /**
+    * creates all possible games.
+    * @return
+    */
+  def mkGames(): Map[Seq[TMove], TicTacToe] = ???
+
+}
+
+/**
+  * Models the well known tic tac toe game.
+  *
+  * The map holds the information which player controls which field.
+  *
+  * The nextplayer parameter defines which player makes the next move.
+  */
+case class TicTacToe(moveHistory: Map[TMove, Player],
+                     nextPlayer: Player = PlayerA) {
+
+  /**
+    * outputs a representation of the tic tac toe like this:
+    *
+    * |---|---|---|
+    * | x | o | x |
+    * |---|---|---|
+    * | x | o | x |
+    * |---|---|---|
+    * | x | o | x |
+    * |---|---|---|
+    *
+    *
+    * @return
+    */
+  def asString(): String = ???
+
+  /**
+    * is true if the game is over.
+    *
+    * The game is over if either of a player wins or there is a draw.
+    */
+  val gameOver = ???
+
+  /**
+    * the moves which are still to be played on this tic tac toe.
+    */
+  val remainingMoves: Set[TMove] = ???
+
+  /**
+    * given a tic tac toe game, this function returns all
+    * games which can be derived by making the next turn. that means one of the
+    * possible turns is taken and added to the set.
+    */
+  lazy val nextGames: Set[TicTacToe] = ???
+
+  /**
+    * Either there is no winner, or PlayerA or PlayerB won the game.
+    *
+    * The set of moves contains all moves which contributed to the result.
+    */
+  def winner: Option[(Player, Set[TMove])] = ???
+
+  /**
+    * returns a copy of the current game, but with the move applied to the tic tac toe game.
+    *
+    * @param move to be played
+    * @param player the player
+    * @return
+    */
+  def turn(p: TMove, player: Player): TicTacToe = ???
+
+}
+
+
---
+++ src/main/scala/fhj/swengb/assignments/ttt/rladstaetter/TicTacToeApp.scala
@@ -0,0 +1,5 @@
+package fhj.swengb.assignments.ttt.rladstaetter
+
+/**
+  * Implement here your TicTacToe JavaFX App.
+  */
